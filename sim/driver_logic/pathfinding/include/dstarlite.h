2fb7854fa32b9b6c182db8899b43a1fd8e409bae
Joshua Wells <joshwells637@gmail.com>
2026-04-08 22:41:15 -0400

    initial pathfinding work

---
+++ sim/driver_logic/pathfinding/include/dstarlite.h
@@ -0,0 +1,17 @@
+#ifndef DSTARLITE_H
+#define DSTARLITE_H
+
+#include <cmath>
+#include <limits>
+#include <vector>
+#include "road.h"
+
+
+
+class DStarLite 
+{
+    private:
+        
+};
+
+#endif
