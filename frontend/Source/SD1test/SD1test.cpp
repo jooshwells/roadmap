e614b6f0b2b23151d6e73c34d432e348d59d1991
Reece <162858922+ReecesPieces124@users.noreply.github.com>
2026-05-02 13:03:32 -0400

    frontend folder (#12)
    * made sample roads
    
    made 2 landscape spline roads. I plan on editting the splines either today or tomorrow
    
    * made the frontend folder for organization
    
    frontend folder
    
    * made sample roads
    
    made 2 landscape spline roads. I plan on editting the splines either today or tomorrow
    
    * made the frontend folder for organization
    tried to solve merge conflicts
    frontend folder
    
    * 2nd commit i suppose (trying to fix the gitignore issue)

---
+++ frontend/Source/SD1test/SD1test.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SD1test.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SD1test, "SD1test" );
